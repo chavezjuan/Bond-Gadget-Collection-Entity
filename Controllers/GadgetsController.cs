3d1c5a490798bbcf88abdf4df1594aa97841ae6f
chavezdev <jctecenge@gmail.com>
2020-07-28 19:12:20 -0300

    Feita a configuracao do contexto com o banco de dados utilizando o EntityFramework. Realizada a primeira migration

---
+++ Controllers/GadgetsController.cs
@@ -0,0 +1,81 @@
+﻿using JamesBondGadgetsEntity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JamesBondGadgetsEntity.Controllers
+{
+    public class GadgetsController : Controller
+    {
+        // Configurando o acesso ao DbContext
+        private ApplicationDbContext context;
+
+        public GadgetsController()
+        {
+            context = new ApplicationDbContext();
+        }
+        //Quando e encerrada a aplicacao ele retira o contexto
+        protected override void Dispose(bool disposing)
+        {
+            context.Dispose();
+        }
+
+        // GET: Gadgets
+        public ActionResult Index()
+        {
+            
+
+            return View("Index");
+        }
+
+        public ActionResult Details(int id)
+        {
+            
+
+            return View("Details");
+        }
+
+        //Retorna um formulário de criação
+        public ActionResult Create()
+        {
+            return View("GadgetForm");
+        }
+
+        public ActionResult Edit(int id)
+        {
+           
+            return View("GadgetForm");
+        }
+
+        public ActionResult Delete(int id)
+        {
+           
+
+            return View("Index");
+        }
+
+        //Cria um item no banco
+        public ActionResult ProcessCreate(GadgetModel gadgetModel)
+        {
+            //Salvando no banco de dados
+            
+
+            return View("Details", gadgetModel);
+        }
+
+        public ActionResult SearchForm()
+        {
+            return View("SearchForm");
+        }
+
+        public ActionResult SearchForName(string searchPhrase)
+        {
+        
+
+
+            return View("Index");
+        }
+    }
+}
---
+++ Models/GadgetModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace JamesBondGadgetsEntity.Models
+{
+    public class GadgetModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        [DisplayName ("Appears in this movie")]
+        public string AppearsIn { get; set; }
+        
+        [Required]
+        public string WithThisActor { get; set; }
+
+        //Criação do construtor da classe quando não for passado nenhum
+        //parâmetro ao instanciar um objeto da classe.
+        public GadgetModel()
+        {
+            Id = -1;
+            Name = "Nothing";
+            Description = "Nothing yet";
+            AppearsIn = "Nowhere";
+            WithThisActor = "With no one";
+        }
+
+        //Agora definimos o construtor com todos os parâmetros
+        public GadgetModel(int id, string name, string description, string appearsIn, string withThisActor)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            AppearsIn = appearsIn;
+            WithThisActor = withThisActor;
+        }
+
+     
+
+
+
+    }
+}
